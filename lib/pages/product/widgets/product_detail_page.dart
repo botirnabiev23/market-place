12c31d2686f957fecb7022f5548ef252d95c5130
prom1 <botnab8@gmail.com>
2025-04-15 17:11:40 +0500

    update product details page

--- lib/pages/product/widgets/product_detail_page.dart
+++ lib/pages/product/widgets/product_detail_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:market_place/models/basket_product_model.dart';
 import 'package:market_place/models/product_model.dart';
+import 'package:market_place/pages/product/widgets/basket_counter.dart';
+import 'package:market_place/providers/basket_provider.dart';
 
-class ProductDetailPage extends StatelessWidget {
+class ProductDetailPage extends ConsumerWidget {
   final Product product;
 
   const ProductDetailPage({super.key, required this.product});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final basketItems = ref.watch(basketProvider);
+    final item = basketItems.firstWhere(
+      (item) => item.product.id == product.id,
+      orElse: () => BasketProduct(product: product, count: 0),
+    );
+
     return Scaffold(
       appBar: AppBar(leading: BackButton()),
       body: Container(
@@ -40,6 +51,63 @@ class ProductDetailPage extends StatelessWidget {
                   Text(
                     product.description,
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
+                  ),
+                  Gap(16),
+                  Row(
+                    children: [
+                      BasketCounter(product: product, count: item.count),
+                      const Gap(10),
+                      Expanded(
+                        child: SizedBox(
+                          height: 48,
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              ref
+                                  .read(basketProvider.notifier)
+                                  .addToBasket(product);
+                              context.pop();
+                            },
+                            style: ButtonStyle(
+                              elevation: WidgetStateProperty.all(0),
+                              foregroundColor:
+                                  WidgetStateProperty.resolveWith<Color>((
+                                    Set<WidgetState> states,
+                                  ) {
+                                    return Colors.white;
+                                  }),
+                              backgroundColor:
+                                  WidgetStateProperty.resolveWith<Color>((
+                                    Set<WidgetState> states,
+                                  ) {
+                                    return Colors.black;
+                                  }),
+                              overlayColor:
+                                  WidgetStateProperty.resolveWith<Color>((
+                                    Set<WidgetState> states,
+                                  ) {
+                                    return Colors.transparent;
+                                  }),
+                              shape: WidgetStateProperty.all<
+                                RoundedRectangleBorder
+                              >(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                              padding: WidgetStateProperty.all(EdgeInsets.zero),
+                            ),
+                            child: Text(
+                              'Add',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
